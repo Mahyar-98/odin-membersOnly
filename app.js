247c489f9a9bfac59190c3dccd2f6f7ccb6599f1
Mahyar <mahyar.erfanian1998@gmail.com>
2024-03-15 11:52:07 -0400

    Connect the database using mongoose and dotenv

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL).catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("hello world");
